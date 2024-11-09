2f4bffe3a7668a145f08873b5e62f170269d83bd
ayanchyaziz123 <aaiz9642@gmail.com>
2024-11-09 11:57:11 -0500

    fixed error

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -1,23 +1,25 @@
+// src/components/Biography.js
 const Biography = () => {
     return (
-      <section id="biography" className="container mx-auto mt-2 p-4 sm:p-6 max-w-6xl">
+      <section id="biography" className="container mx-auto mt-2 p-4 sm:p-6  max-w-6xl">
         <h2 className="text-3xl font-extrabold text-center mb-6" style={{ color: '#996232' }}>
           Biography
         </h2>
         <p className="text-gray-700 leading-relaxed">
-          <span className="text-2xl">Hi there!</span> I&apos;m Rahman Azizur Ayan, a dedicated software engineer with a passion for the cosmos. Welcome to my space where technology meets the stars! I have a strong foundation in data structures, algorithms, and object-oriented programming, and I thrive on tackling complex challenges to bring innovative ideas to life.
+          <span className="text-2xl">Hi there!</span> Hi there! I&apos;m Rahman Azizur Ayan, a dedicated software engineer with a passion for the cosmos. Welcome to my space where technology meets the stars! I have a strong foundation in data structures, algorithms, and object-oriented programming, and I thrive on tackling complex challenges to bring innovative ideas to life.
         </p>
         <p className="text-gray-700 leading-relaxed mt-4">
-          My experience spans diverse domains, from developing advanced eCommerce platforms using Django, React, MySQL, and Machine Learning to implementing optimized algorithms in high-performance software solutions. I’m driven by a desire to innovate and explore, continuously learning to stay at the forefront of technology trends, especially in AI, data science, and web development.
+          My experience spans diverse domains, from developing advanced eCommerce platforms using Django, React, MySQL, and Machine Learning to implementing optimized algorithms in high-performance software solutions. I&apos;m driven by a desire to innovate and explore, continuously learning to stay at the forefront of technology trends, especially in AI, data science, and web development.
         </p>
         <p className="text-gray-700 leading-relaxed mt-4">
-          I’m also a strong advocate for collaboration, valuing the synergy of working with talented professionals who share a passion for problem-solving. Let’s connect, explore ideas, and work together to drive success in the world of technology and computer science.
+          I&apos;m also a strong advocate for collaboration, valuing the synergy of working with talented professionals who share a passion for problem-solving. Let&apos;s connect, explore ideas, and work together to drive success in the world of technology and computer science.
         </p>
         <p className="text-gray-700 leading-relaxed mt-4">
-          Through this blog, I document my journey and insights at the intersection of technology and astronomy. Thank you for visiting, and feel free to reach out—let's embark on a journey of shared growth and discovery!
+          Through this blog, I document my journey and insights at the intersection of technology and astronomy. Thank you for visiting, and feel free to reach out&mdash;let&apos;s embark on a journey of shared growth and discovery!
         </p>
       </section>
     );
-};
-
-export default Biography;
+  };
+  
+  export default Biography;
+  
